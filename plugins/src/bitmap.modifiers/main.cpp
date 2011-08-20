647719b714fc08f8fb2d3e3c54fde09465069808
jaw <jaw@anemone.(none)>
2011-08-20 22:41:37 +0200

    - moved add_noise module to texture modifiers - added perlin noise module

--- plugins/src/bitmap.modifiers/main.cpp
+++ plugins/src/bitmap.modifiers/main.cpp
@@ -8,6 +8,12 @@
 #include "main.h"
 
 
+#ifndef _WIN32
+#include <unistd.h>
+#define Sleep sleep
+#endif
+
+
 class module_texture_to_bitmap : public vsx_module {
   // in
 	vsx_module_param_texture* texture_in;
@@ -213,6 +219,153 @@ public:
 };
 
 
+class module_bitmap_add_noise : public vsx_module {
+  // in
+  vsx_module_param_bitmap* bitmap_in;
+  float time;
+
+  // out
+  vsx_module_param_bitmap* result1;
+  // internal
+  vsx_bitmap* bitm;
+  vsx_bitmap t_bitm;
+
+  int buf, frame;
+  unsigned long *data_a;
+  unsigned long *data_b;
+  int bitm_timestamp;
+  vsx_bitmap result_bitm;
+  bool first, worker_running, t_done;
+  pthread_t         worker_t;
+  pthread_attr_t    worker_t_attr;
+  int my_ref;
+
+public:
+
+
+
+static void* noise_worker(void *ptr) {
+  int i_frame = -1;
+  //int x,y;
+  bool buf = false;
+  unsigned long *p;
+  while (((module_bitmap_add_noise*)ptr)->worker_running) {
+    if (i_frame != ((module_bitmap_add_noise*)ptr)->frame) {
+    //printf("%d ",ptr);
+      // time to run baby
+      if (buf) p = ((module_bitmap_add_noise*)ptr)->data_a;
+      else p = ((module_bitmap_add_noise*)ptr)->data_b;
+
+      unsigned long b_c = ((module_bitmap_add_noise*)ptr)->result_bitm.size_x * ((module_bitmap_add_noise*)ptr)->result_bitm.size_y;
+
+      //((module_bitmap_add_noise*)ptr)->result_bitm->data;
+      //unsigned long cc = rand()<<8 | (char)rand();
+      for (unsigned long x = 0; x < b_c; ++x) {
+          p[x] = ((module_bitmap_add_noise*)ptr)->t_bitm.data[x] | rand() << 8  | (unsigned char)rand(); //bitm->data[x + y*result_bitm.size_x]
+          //((module_bitmap_add_noise*)ptr)->bitm->data[x]<<
+      }
+      ((module_bitmap_add_noise*)ptr)->result_bitm.valid = true;
+      ((module_bitmap_add_noise*)ptr)->result_bitm.data = p;
+      ++((module_bitmap_add_noise*)ptr)->result_bitm.timestamp;
+      buf = !buf;
+      i_frame = ((module_bitmap_add_noise*)ptr)->frame;
+    }
+    Sleep(100);
+  }
+  ((module_bitmap_add_noise*)ptr)->t_done = true;
+  return 0;
+}
+
+void module_info(vsx_module_info* info)
+{
+  info->identifier = "bitmaps;modifiers;add_noise";
+  info->in_param_spec = "bitmap_in:bitmap";
+  info->out_param_spec = "bitmap:bitmap";
+  info->component_class = "bitmap";
+  info->description = "";
+}
+
+// ----
+
+void declare_params(vsx_module_param_list& in_parameters, vsx_module_param_list& out_parameters)
+{
+  bitmap_in = (vsx_module_param_bitmap*)in_parameters.create(VSX_MODULE_PARAM_ID_BITMAP,"bitmap_in");
+  result1 = (vsx_module_param_bitmap*)out_parameters.create(VSX_MODULE_PARAM_ID_BITMAP,"bitmap");
+  result_bitm.size_x = 0;
+  result_bitm.size_y = 0;
+  result_bitm.bpp = 4;
+  result_bitm.bformat = GL_RGBA;
+  my_ref = 0;
+  result_bitm.ref = &my_ref;
+  first = true;
+  worker_running = false;
+  buf = 0;
+  frame = 0;
+  t_done = false;
+}
+
+void run() {
+  bitm = bitmap_in->get_addr();
+  if (bitm) {
+    t_bitm = *bitm;
+    if (result_bitm.size_x != bitm->size_x && result_bitm.size_y != bitm->size_y) {
+      if (worker_running)
+      pthread_join(worker_t,0);
+      worker_running = false;
+
+      // need to realloc
+      if (result_bitm.valid) delete[] result_bitm.data;
+      data_a = new unsigned long[bitm->size_x*bitm->size_y];
+      data_b = new unsigned long[bitm->size_x*bitm->size_y];
+      result_bitm.data = data_a;
+      result_bitm.valid = true;
+      result_bitm.size_x = bitm->size_x;
+      result_bitm.size_y = bitm->size_y;
+
+      pthread_attr_init(&worker_t_attr);
+      pthread_create(&worker_t, &worker_t_attr, &noise_worker, (void*)this);
+      //pthread_detach(worker_t);
+      /*pthread_getschedparam (worker_t,&policy,&s_param)*/;
+      sched_param s_param;
+      int policy = 0;
+      s_param.sched_priority = 20;
+      pthread_setschedparam (worker_t,policy,&s_param);
+      worker_running = true;
+    }
+    ++frame;
+    //if (bitm->valid)
+    //if (first) {
+/*      for (int x = 0; x < result_bitm.size_x; ++x) {
+        for (int y = 0; y < result_bitm.size_y; ++y) {
+          result_bitm.data[x + y*result_bitm.size_x] = bitm->data[x + y*result_bitm.size_x] | (unsigned char)rand() << 24  | (unsigned char)rand() << 8 | (unsigned char)rand() << 16 | (unsigned char)rand(); //bitm->data[x + y*result_bitm.size_x]
+        }
+      }
+      first = false;
+      ++result_bitm.timestamp;*/
+    //}
+
+
+    result1->set_p(result_bitm);
+  } else {
+    worker_running = false;
+    pthread_join(worker_t,0);
+    result1->valid = false;
+  }
+}
+
+void on_delete() {
+  if (worker_running) {
+    worker_running = false;
+    result1->valid = false;
+    pthread_join(worker_t,0);
+  }
+  delete[] data_a;
+  delete[] data_b;
+  if (result_bitm.valid)
+  delete[] result_bitm.data;
+}
+
+};
 
 
 
@@ -225,6 +378,8 @@ vsx_module* create_new_module(unsigned long module) {
   switch(module) {
     case 0: return (vsx_module*)(new module_texture_to_bitmap);
     case 1: return (vsx_module*)(new module_bitmap_to_particlesystem);
+    case 2: return (vsx_module*)(new module_bitmap_add_noise);
+
   }
   return 0;
 }
@@ -233,13 +388,14 @@ void destroy_module(vsx_module* m,unsigned long module) {
   switch(module) {
     case 0: delete (module_texture_to_bitmap*)m; break;
     case 1: delete (module_bitmap_to_particlesystem*)m; break;
+    case 2: delete (module_bitmap_add_noise*)m; break;
   }
 }
 
 
 unsigned long get_num_modules() {
   // we have only one module. it's id is 0
-  return 2;
+  return 3;
 }  
 
 
--- plugins/src/bitmap.modifiers/main.h
+++ plugins/src/bitmap.modifiers/main.h
@@ -7,40 +7,6 @@
 # define DLLIMPORT __declspec (dllimport)
 #endif /* Not BUILDING_DLL */
 
-
-
-// ******************************************************
-
-class module_bitmap_add_noise : public vsx_module {
-  // in
-	vsx_module_param_bitmap* bitmap_in;
-	float time;
-	
-	// out
-	vsx_module_param_bitmap* result1;
-	// internal
-	vsx_bitmap* bitm;
-  vsx_bitmap t_bitm;
-
-	int buf, frame;
-	unsigned long *data_a;
-	unsigned long *data_b;
-	int bitm_timestamp;
-	vsx_bitmap result_bitm;
-	bool first, worker_running, t_done;
-  pthread_t					worker_t;
-  pthread_attr_t		worker_t_attr;
-	
-public:
-  virtual void module_info(vsx_module_info* info);
-	virtual void declare_params(vsx_module_param_list& in_parameters, vsx_module_param_list& out_parameters);
-	virtual void run();
-	virtual void start();
-	virtual void stop();
-	void on_delete();
-};
-
-
 extern "C" {
 _declspec(dllexport) vsx_module* create_new_module(unsigned long module);
 _declspec(dllexport) void destroy_module(vsx_module* m,unsigned long module);
