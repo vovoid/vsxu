2d6717c36036c7d1709f2f6c2a3eea76b8f87c20
Jonatan Wallmander <jaw@eris.vovoid>
2011-12-13 11:15:56 +0100

    Fixed up plugin documentation Cleaned up indentation of vsx_param.h

--- doc/development/plugin_template/main.cpp
+++ doc/development/plugin_template/main.cpp
@@ -1,180 +1,221 @@
-#include "vsxfst.h"
-#include "vsx_param.h"
-#include "vsx_module.h"
-#include "main.h"
-#include "vsx_math_3d.h"
-
-
+#include <vsxfst.h> // file system and string functions
+#include <vsx_param.h> // parameter definitions
+#include <vsx_module.h> // module base class + definition
+#include <vsx_math_3d.h> // vsx_vector and matrix maths
 
 class vsx_module_template : public vsx_module {
   // in
-	vsx_module_param_float3* position;
-	vsx_module_param_float3* size;
-	vsx_module_param_float* angle;
-	vsx_module_param_float3* rotation_axis;
-	vsx_module_param_float4* color_rgb;
-	vsx_module_param_int* border;
-	// out
-	vsx_module_param_render* render_result;
-	// internal
+  vsx_module_param_float3* position;
+  vsx_module_param_float3* size;
+  vsx_module_param_float* angle;
+  vsx_module_param_float3* rotation_axis;
+  vsx_module_param_float4* color_rgb;
+  vsx_module_param_int* border;
+  // out
+  vsx_module_param_render* render_result;
+  // internal
 
 public:
 
   void module_info(vsx_module_info* info)
   {
     info->identifier = "templates;simple";
+    info->description = 
+      "Template module showing all\n"
+      "possible data types and tricks\n"
+      "you can do in modules.\n"
+    ;
     /*
-     * Parameter syntax:
+     * Parameter specifications
+     *   Used to tell artiste what parameters we want in the GUI and how 
+     *   those should behave.
+     * 
+     * Syntax:
      * 
      * [name]:[type][?[flag][=value]]
      * 
-     * Name and type are required.
-     * Flag can be many things, controllers or options on how the GUI should
-     * treat it.
+     * - Name and type are required.
+     * - Flag can be many things, controllers or options on how the GUI should
+     *   treat it.
      * 
-     * NonConnection flag:
-     * 		nc=1
+     * To prevent connections to a parameter, add the non-connection flag:
+     * nc=1
      * 
-     * 		This will make it impossible to connect the parameter to any other 
-     * 		module. That is, only the human operating VSXu can set it. 
-     * 		Use this when you do operations that involve large chunks of memory
-     *    or heavy one-time precalculations.
+     * This will make it impossible to connect the parameter to any other 
+     * module. That is, only the human operating VSXu can set it. 
+     * Use this when you do operations that involve large chunks of memory
+     * or heavy one-time precalculations.
      * 
-     * Setting Default Controller flags (opened when double-clicking the param):
-     * 		default_controller=[value]
+     * Setting "Default Controller" (which controller is displayed when double-
+     *  clicking the param):
+     *   default_controller=[value]
      * 
-     * 		Different values (controllers) for different parameters are available:
+     * Different values (controllers) for different parameters are available:
      *  
-     *  	controller_knob				float
-     *		controller_slider			float, float3, float4
-     *  	controller_edit  			string/resource  
-     *		controller_resource 	resource (to use it with strings, make it default)
+     *   controller_knob        float
+     *   controller_slider      float, float3, float4
+     *   controller_edit        string/resource  
+     *   controller_resource    resource (to use it with strings, make it 
+     *                          default)
      * 
-     * 		Example:
-     * 		  To make a knob default (when double-clicking) go like:
-     * 		  angle:float?default_controller=controller_knob
-     *  
-     *  
-     *   
+     * Example:
+     *   To make a knob default (when double-clicking) go like this:
+     *   angle:float?default_controller=controller_knob
      * 
      */
-    info->description = "\
-Template module showing all\n\
-possible data types and tricks\n\
-you can do in modules.\n\
-";
-  /*
-  dialog_Test1=(\
-    comp=false&\
-    size_x=7&\
-    size_y=4&\
-    control_knob=(\
-      pos_x=-0.5&\
-      pos_y=0&\
-      param=size&\
-      type=float&\
-      hint=MyHint\
-    )&\
-    control_slider=(\
-      pos_x=0.5&\
-      pos_y=0&\
-      param=angle&\
-      type=float\
-    )\
-  )\
-  */
-  /*// &min=0&help=`This controls the angle of the line\nrotated around the [rotation_axis].\nHint: Hook it to an oscillator or similar\nfor some fun twisting action!`,\*/
-//  info->in_param_spec = "spatial:complex{position:float3,angle:float,rotation_axis:float3,size:float3},color:float4";
-    info->in_param_spec = "\
-spatial:complex{position:float3,angle:float,rotation_axis:float3,size:float3,border:enum?no|yes},color:float4";
+    info->in_param_spec = 
+      "spatial:complex{"
+        "position:float3,"
+        "angle:float,"
+        "rotation_axis:float3,"
+        "size:float3,"
+        "border:enum?no|yes
+      "},"
+      "color:float4"
+    ;
   
     info->out_param_spec = "render_out:render";
     info->component_class = "render";
   }
 
-
-  void declare_params(vsx_module_param_list& in_parameters, vsx_module_param_list& out_parameters)
+  // this method uses the engine's parameter holder to create data holders for 
+  // each of the parameters also binding them to their name (string)
+  void declare_params(
+    vsx_module_param_list& in_parameters, 
+    vsx_module_param_list& out_parameters
+  )
   {
     loading_done = true;
-  	position = (vsx_module_param_float3*)in_parameters.create(VSX_MODULE_PARAM_ID_FLOAT3, "position");
-  	position->set(0.0f, 0);
-  	position->set(0.0f, 1);
-  	position->set(0.0f, 2);
-	
-  	size = (vsx_module_param_float3*)in_parameters.create(VSX_MODULE_PARAM_ID_FLOAT3, "size");
-  	size->set(1.0f,0);
-  	size->set(0.3f,1);
-  	angle = (vsx_module_param_float*)in_parameters.create(VSX_MODULE_PARAM_ID_FLOAT, "angle");
-  	angle->set(0.0f);
+    position = (vsx_module_param_float3*)in_parameters.create(
+      VSX_MODULE_PARAM_ID_FLOAT3, 
+      "position"
+    );
+    position->set(0.0f, 0);
+    position->set(0.0f, 1);
+    position->set(0.0f, 2);
 
-  	border = (vsx_module_param_int*)in_parameters.create(VSX_MODULE_PARAM_ID_INT, "border");
-	
-  	rotation_axis = (vsx_module_param_float3*)in_parameters.create(VSX_MODULE_PARAM_ID_FLOAT3, "rotation_axis");
-  	rotation_axis->set(1.0f, 0);
-  	rotation_axis->set(1.0f, 1);
-  	rotation_axis->set(0.0f, 2);
-    color_rgb = (vsx_module_param_float4*)in_parameters.create(VSX_MODULE_PARAM_ID_FLOAT4, "color");
+    size = (vsx_module_param_float3*)in_parameters.create(
+      VSX_MODULE_PARAM_ID_FLOAT3, 
+      "size"
+    );
+    size->set(1.0f,0);
+    size->set(0.3f,1);
+    angle = (vsx_module_param_float*)in_parameters.create(
+      VSX_MODULE_PARAM_ID_FLOAT, 
+      "angle"
+    );
+    angle->set(0.0f);
+
+    border = (vsx_module_param_int*)in_parameters.create(
+      VSX_MODULE_PARAM_ID_INT, 
+      "border"
+    );
+
+    rotation_axis = (vsx_module_param_float3*)in_parameters.create(
+      VSX_MODULE_PARAM_ID_FLOAT3, 
+      "rotation_axis"
+    );
+    rotation_axis->set(1.0f, 0);
+    rotation_axis->set(1.0f, 1);
+    rotation_axis->set(0.0f, 2);
+    color_rgb = (vsx_module_param_float4*)in_parameters.create(
+      VSX_MODULE_PARAM_ID_FLOAT4, 
+      "color"
+    );
     color_rgb->set(1.0,0);
     color_rgb->set(1.0,1);
     color_rgb->set(1.0,2);
     color_rgb->set(1.0,3);
 
-  	render_result = (vsx_module_param_render*)out_parameters.create(VSX_MODULE_PARAM_ID_RENDER,"render_out");
-  	render_result->set(0);
-  }	
+    render_result = (vsx_module_param_render*)out_parameters.create(
+      VSX_MODULE_PARAM_ID_RENDER,
+      "render_out"
+    );
+    render_result->set(0);
+  }
+  
+  // this is run once per frame/iteration of the whole engine
+  void run()
+  {
+    // procrastinate
+  }
 
-  void output(vsx_module_param_abs* param) {
-  	glMatrixMode(GL_MODELVIEW);
-  	glPushMatrix();
-  	glTranslatef(position->get(0),position->get(1),position->get(2));
+  // this is run for each connection to this in-param.
+  void output(vsx_module_param_abs* param) 
+  {
+    glMatrixMode(GL_MODELVIEW);
+    glPushMatrix();
+    // translation
+    glTranslatef(position->get(0),position->get(1),position->get(2));
+    // rotation
+    glRotatef(
+      (float)angle->get()*360, 
+      rotation_axis->get(0), 
+      rotation_axis->get(1), 
+      rotation_axis->get(2)
+    );
+    // scaling
+    glScalef(size->get(0), size->get(1), size->get(2));
+    // color
+    glColor4f(
+      color_rgb->get(0),
+      color_rgb->get(1),
+      color_rgb->get(2),
+      color_rgb->get(3)
+    );
 
-  	glRotatef((float)angle->get()*360, rotation_axis->get(0), rotation_axis->get(1), rotation_axis->get(2));
-
-  	glScalef(size->get(0), size->get(1), size->get(2));
-
-  	glColor4f(color_rgb->get(0),color_rgb->get(1),color_rgb->get(2),color_rgb->get(3));
-
-  	glBegin(GL_QUADS);
-  	  glTexCoord2f(0.0f,0.0f);
+    glBegin(GL_QUADS);
+      glTexCoord2f(0.0f,0.0f);
       glVertex3f(-1.0f, -1.0f, 0.0f);
-  	  glTexCoord2f(0.0f,1.0f);
+      glTexCoord2f(0.0f,1.0f);
       glVertex3f(-1.0f,  1.0f, 0.0f);
-  	  glTexCoord2f(1.0f,1.0f);
+      glTexCoord2f(1.0f,1.0f);
       glVertex3f( 1.0f,  1.0f, 0.0f);
-  	  glTexCoord2f(1.0f,0.0f);
+      glTexCoord2f(1.0f,0.0f);
       glVertex3f( 1.0f, -1.0f, 0.0f);
-  	glEnd();
-  	
-  	if (border->get()) {
-    	glEnable(GL_LINE_SMOOTH);
-    	glLineWidth(1.5);
-    	glEnable(GL_BLEND);
-    	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    
-    	glBegin(GL_LINE_STRIP);
-    		glColor3f(0, 0, 0);
-    		glVertex3f(-2, -0.4f, 0);
-    		glVertex3f(-2, -0.2f, 0);
-    		glVertex3f( 2, -0.2f, 0);
-    		glVertex3f( 2, -0.4f, 0);
-    		glVertex3f(-2, -0.4f, 0);
-    	glEnd();
+    glEnd();
+
+    if (border->get()) 
+    {
+      glEnable(GL_LINE_SMOOTH);
+      glLineWidth(1.5);
+      glEnable(GL_BLEND);
+      glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+      glBegin(GL_LINE_STRIP);
+        glColor3f(0, 0, 0);
+        glVertex3f(-2, -0.4f, 0);
+        glVertex3f(-2, -0.2f, 0);
+        glVertex3f( 2, -0.2f, 0);
+        glVertex3f( 2, -0.4f, 0);
+        glVertex3f(-2, -0.4f, 0);
+      glEnd();
     }
 
-  	glPopMatrix();
+    glPopMatrix();
     render_result->set(1);
     loading_done = true;
   }
-
 };
 
 
 
-//////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////
+//******************************************************************************
+//*** F A C T O R Y ************************************************************
+//******************************************************************************
 
-vsx_module* create_new_module(unsigned long module) {
+#ifndef _WIN32
+#define __declspec(a)
+#endif
+
+extern "C" {
+__declspec(dllexport) vsx_module* create_new_module(unsigned long module);
+__declspec(dllexport) void destroy_module(vsx_module* m,unsigned long module);
+__declspec(dllexport) unsigned long get_num_modules();
+}
+
+
+vsx_module* MOD_CM(unsigned long module) {
   switch (module) {
     case 0: return (vsx_module*)new vsx_module_template;
   } // switch
@@ -182,15 +223,13 @@ vsx_module* create_new_module(unsigned long module) {
 }
 
 
-
-void destroy_module(vsx_module* m,unsigned long module) {
+void MOD_DM(vsx_module* m,unsigned long module) {
   switch(module) {
     case 0: delete (vsx_module_template*)m; break;
   }
 }
 
 
-unsigned long get_num_modules() {
-  return 2;
+unsigned long MOD_NM() {
+  return 1;
 }  
-
--- engine/include/vsx_param.h
+++ engine/include/vsx_param.h
@@ -23,7 +23,7 @@
 #define VSX_PARAM_H
 
 #if BUILDING_DLL
-	#ifndef WIN32_LEAN_AND_MEAN
+  #ifndef WIN32_LEAN_AND_MEAN
   #define WIN32_LEAN_AND_MEAN
   #endif
   //#include <windows.h>
@@ -50,7 +50,7 @@
   #include <map>
 #endif
 #ifndef VSX_NO_GL
-	#define VSX_NOGLUT_LOCAL
+  #define VSX_NOGLUT_LOCAL
   #include "vsx_gl_global.h"
   #undef VSX_NOGLUT_LOCAL
   #include "vsx_texture_info.h"
@@ -101,18 +101,18 @@ class vsx_module_param_abs;
 class vsx_param_vsxl_abs;
 class vsx_module_param_abs {
 public:
-	void* vsxl_modifier;
+  void* vsxl_modifier;
 
-	int type;
-	vsx_string name;
-	unsigned long updates; // number of writes, incremented each time the parameter is written to
-	vsx_module_param_abs(const char* nname) : vsxl_modifier(0),name(nname),updates(0),valid(false),value_from_module(false) {}
-	bool valid;
-	bool value_from_module;
-	bool critical; // is this critical for whatever purpouse it exists?
-	bool all_required; // when multiple stuff connected to this, do all have to return true in order to run the module?
+  int type;
+  vsx_string name;
+  unsigned long updates; // number of writes, incremented each time the parameter is written to
+  vsx_module_param_abs(const char* nname) : vsxl_modifier(0),name(nname),updates(0),valid(false),value_from_module(false) {}
+  bool valid;
+  bool value_from_module;
+  bool critical; // is this critical for whatever purpouse it exists?
+  bool all_required; // when multiple stuff connected to this, do all have to return true in order to run the module?
 
-	const vsx_string& get_name() const {return name;}
+  const vsx_string& get_name() const {return name;}
 
   virtual void set_current_as_default() = 0;
   virtual void set_default() = 0;
@@ -126,16 +126,16 @@ template<int id, typename T, int arity = 1, int clear_ram = 0>
 
 class vsx_module_param : public vsx_module_param_abs {
 public:
-	// the data on module level
-	T* param_data;
-	// the value the GUI/other components assign this 
-	T* param_data_suggestion;
-	// default suggestion value
-	T* param_data_default;
-	
+  // the data on module level
+  T* param_data;
+  // the value the GUI/other components assign this 
+  T* param_data_suggestion;
+  // default suggestion value
+  T* param_data_default;
+  
   bool default_set;
 
-	vsx_module_param(const char* p_name) : 
+  vsx_module_param(const char* p_name) : 
     vsx_module_param_abs(p_name), 
     param_data(0),
     param_data_suggestion(0),
@@ -143,20 +143,20 @@ public:
     default_set(false) {}
 
 
-	void check_free()	{
+  void check_free()	{
     //printf("check free\n");
-		if(!param_data) {
-			param_data = new T[arity];
-			param_data_default = new T[arity];
-			param_data_suggestion = new T[arity];
+    if(!param_data) {
+      param_data = new T[arity];
+      param_data_default = new T[arity];
+      param_data_suggestion = new T[arity];
       if (clear_ram)
       {
         memset((void*)param_data_default,0,sizeof(T)*arity);
         memset((void*)param_data,0,sizeof(T)*arity);
         memset((void*)param_data_suggestion,0,sizeof(T)*arity);
       }
-		}
-	}
+    }
+  }
 
 
   void set_current_as_default() {
@@ -181,33 +181,33 @@ public:
   // MODULE PARAMETER METHODS
   // -------------------------------------------------------------------------------------------
   // this method is only used by the modules 
-	void set(T val, int index = 0) {
-		check_free();
-		param_data[index] = val;
-		param_data_suggestion[index] = val;
-		valid = true;
-		value_from_module = true;
-	}
-	// this method is only used by the modules 
-	void set_p(T& val, int index = 0) {
+  void set(T val, int index = 0) {
     check_free();
     param_data[index] = val;
     param_data_suggestion[index] = val;
     valid = true;
     value_from_module = true;
   }
-	
-	T* get_addr() {
+  // this method is only used by the modules 
+  void set_p(T& val, int index = 0) {
+    check_free();
+    param_data[index] = val;
+    param_data_suggestion[index] = val;
+    valid = true;
+    value_from_module = true;
+  }
+  
+  T* get_addr() {
     if (valid)
     return param_data;
     else
     return 0;
   }
-	
-	T& get(int index = 0) {
+  
+  T& get(int index = 0) {
     return param_data[index];
   }
-	
+  
   // -------------------------------------------------------------------------------------------
   // ENGINE PARAMETER METHODS
   // -------------------------------------------------------------------------------------------
@@ -215,29 +215,29 @@ public:
   // it runs that and lets it set the destination value instead.
   // this means for a float3 that the modifier will be run 3 times but that's all good if you
   // want to recalculate normals or whatever with it.
-	void set_internal(T val, int index = 0) {
-		check_free();
-		param_data_suggestion[index] = val;
-		if (!vsxl_modifier) {
-		  param_data[index] = val;
+  void set_internal(T val, int index = 0) {
+    check_free();
+    param_data_suggestion[index] = val;
+    if (!vsxl_modifier) {
+      param_data[index] = val;
     }
-	}
+  }
 
-	void set_internal_from_param(vsx_module_param* val) {
-		if (!val->valid) {
+  void set_internal_from_param(vsx_module_param* val) {
+    if (!val->valid) {
       valid = false;
       return;
     }
-		check_free();
+    check_free();
     for (int i = 0; i < arity; ++i) {
-  		param_data_suggestion[i] = val->param_data[i];
-  		if (!vsxl_modifier) {
-  		  param_data[i] = val->param_data[i];
+      param_data_suggestion[i] = val->param_data[i];
+      if (!vsxl_modifier) {
+        param_data[i] = val->param_data[i];
       }		
     }
     valid = true;
     return;
-	}
+  }
 
   bool set_internal_from_param_with_value_check(vsx_module_param* val) {
     if (!val->valid) {
@@ -258,22 +258,22 @@ public:
     return changed;
   }
 
-	void set_raw(T val, int index = 0) {
-		param_data[index] = val;
-	}
+  void set_raw(T val, int index = 0) {
+    param_data[index] = val;
+  }
 
-	T get_internal(int index = 0) {
-	  return param_data_suggestion[index];
-	}
+  T get_internal(int index = 0) {
+    return param_data_suggestion[index];
+  }
 
-	~vsx_module_param() {
+  ~vsx_module_param() {
     if (param_data_suggestion)
     delete[] param_data_suggestion;
     if (param_data_default)
     delete[] param_data_default;
     if (param_data)
     delete[] param_data;
-	}
+  }
 };
 
 //////////////////////////////////////////////////////////////////////
@@ -285,33 +285,33 @@ public:
   ~vsx_module_param_list();
 };
 
-typedef vsx_module_param<0, int,							1,1 > vsx_module_param_int; // use get() set()
-typedef vsx_module_param<1, int,							1,1 > vsx_module_param_render; // use get() set()
-typedef vsx_module_param<0, float,						1,1 > vsx_module_param_float; // use get() set()
-typedef vsx_module_param<0, float,						3,1> vsx_module_param_float3; // use get() set()
-typedef vsx_module_param<0, double,						1,1 > vsx_module_param_double; // use get() set()
-typedef vsx_module_param<0, vsx_string,				1,0 > vsx_module_param_string; // use get() set()
+typedef vsx_module_param<0, int,                 1,1 > vsx_module_param_int; // use get() set()
+typedef vsx_module_param<1, int,                 1,1 > vsx_module_param_render; // use get() set()
+typedef vsx_module_param<0, float,               1,1 > vsx_module_param_float; // use get() set()
+typedef vsx_module_param<0, float,               3,1> vsx_module_param_float3; // use get() set()
+typedef vsx_module_param<0, double,              1,1 > vsx_module_param_double; // use get() set()
+typedef vsx_module_param<0, vsx_string,          1,0 > vsx_module_param_string; // use get() set()
 #ifdef VSX_NO_GL
-  typedef vsx_module_param<0, void*,              1,1 > vsx_module_param_texture; 
+  typedef vsx_module_param<0, void*,             1,1 > vsx_module_param_texture; 
 #else
   #ifndef VSX_NO_TEXTURE
-    typedef vsx_module_param<0, vsx_texture*,	      1,0 > vsx_module_param_texture; // use get() set()
+    typedef vsx_module_param<0, vsx_texture*,    1,0 > vsx_module_param_texture; // use get() set()
   #else
-    typedef vsx_module_param<0, void*,              1,1 > vsx_module_param_texture;
+    typedef vsx_module_param<0, void*,           1,1 > vsx_module_param_texture;
   #endif
 #endif
-typedef vsx_module_param<0, float,							 4,1 > vsx_module_param_float4; // use get() set()
-typedef vsx_module_param<0, vsx_matrix,					 1,0 > vsx_module_param_matrix; // use get() set()
-typedef vsx_module_param<0, vsx_mesh*,		  	   1,0 > vsx_module_param_mesh; // use get() / set()
-typedef vsx_module_param<0, vsx_bitmap,					 1,0 > vsx_module_param_bitmap; // use get_addr() / set_p()
-typedef vsx_module_param<0, vsx_particlesystem,	 1,0 > vsx_module_param_particlesystem; // use get_addr() / set_p()
-typedef vsx_module_param<0, vsx_float_array,	   1,0 > vsx_module_param_float_array; // use get_addr() set_p()
-typedef vsx_module_param<0, vsx_sequence,   	   1,0 > vsx_module_param_sequence; // use get() set()
+typedef vsx_module_param<0, float,               4,1 > vsx_module_param_float4; // use get() set()
+typedef vsx_module_param<0, vsx_matrix,          1,0 > vsx_module_param_matrix; // use get() set()
+typedef vsx_module_param<0, vsx_mesh*,           1,0 > vsx_module_param_mesh; // use get() / set()
+typedef vsx_module_param<0, vsx_bitmap,          1,0 > vsx_module_param_bitmap; // use get_addr() / set_p()
+typedef vsx_module_param<0, vsx_particlesystem,  1,0 > vsx_module_param_particlesystem; // use get_addr() / set_p()
+typedef vsx_module_param<0, vsx_float_array,     1,0 > vsx_module_param_float_array; // use get_addr() set_p()
+typedef vsx_module_param<0, vsx_sequence,        1,0 > vsx_module_param_sequence; // use get() set()
 typedef vsx_module_param<0, vsx_2dgrid_mesh,     1,0 > vsx_module_param_segment_mesh; // use get_addr() / set_p()
-typedef vsx_module_param<0, vsx_abstract,   	   1,0 > vsx_module_param_abstract; // use get_addr() / set_p()
-typedef vsx_module_param<1, float,   	           4,1 > vsx_module_param_quaternion; // use get_addr() / set_p()
-typedef vsx_module_param<1, vsx_string,   	     1,0 > vsx_module_param_resource; // use get() / set()
-typedef vsx_module_param<0, vsx_float3_array,	   1,0 > vsx_module_param_float3_array; // use get_addr() set_p()
+typedef vsx_module_param<0, vsx_abstract,        1,0 > vsx_module_param_abstract; // use get_addr() / set_p()
+typedef vsx_module_param<1, float,               4,1 > vsx_module_param_quaternion; // use get_addr() / set_p()
+typedef vsx_module_param<1, vsx_string,          1,0 > vsx_module_param_resource; // use get() / set()
+typedef vsx_module_param<0, vsx_float3_array,    1,0 > vsx_module_param_float3_array; // use get_addr() set_p()
 typedef vsx_module_param<0, vsx_quaternion_array,1,0 > vsx_module_param_quaternion_array; // use get_addr() set_p()
 
 #endif
