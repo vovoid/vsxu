4c85131a595a979ceadec95167a56f6b1a0ff928
Jonatan "jaw" Wallmander <jonatan@vovoid.com>
2012-11-01 21:17:48 +0100

    New module: mesh scale normalize - make sure a loaded mesh has a constant scale. Scale can be set with parameters, but most of the time the default settings should suffice. (Values from 0..1)

--- plugins/src/mesh.modifiers/main.cpp
+++ plugins/src/mesh.modifiers/main.cpp
@@ -872,6 +872,144 @@ public:
 };
 
 
+class vsx_module_mesh_scale_normalize : public vsx_module {
+  // in
+  vsx_module_param_mesh* mesh_in;
+  // out
+  vsx_module_param_mesh* mesh_out;
+  // internal
+  vsx_mesh* mesh;
+public:
+  bool init() {
+    mesh = new vsx_mesh;
+    return true;
+  }
+
+  void on_delete()
+  {
+    delete mesh;
+  }
+  void module_info(vsx_module_info* info)
+  {
+    info->identifier = "mesh;modifiers;transforms;mesh_norm_scale";
+    info->description = "Scales mesh to within 0..1 size";
+    info->in_param_spec = "mesh_in:mesh";
+    info->out_param_spec = "mesh_out:mesh";
+    info->component_class = "mesh";
+  }
+
+  void declare_params(vsx_module_param_list& in_parameters, vsx_module_param_list& out_parameters)
+  {
+    mesh_in = (vsx_module_param_mesh*)in_parameters.create(VSX_MODULE_PARAM_ID_MESH,"mesh_in");
+    loading_done = true;
+    mesh_out = (vsx_module_param_mesh*)out_parameters.create(VSX_MODULE_PARAM_ID_MESH,"mesh_out");
+  }
+
+  unsigned long prev_timestamp;
+  vsx_vector v;
+  void run() {
+    vsx_mesh** p = mesh_in->get_addr();
+    if (!p)
+    {
+      mesh_empty.timestamp = (int)(engine->real_vtime*1000.0f);
+      mesh_out->set(&mesh_empty);
+      prev_timestamp = 0xFFFFFFFF;
+      return;
+    }
+
+    if (p && (param_updates || prev_timestamp != (*p)->timestamp)) {
+      prev_timestamp = (*p)->timestamp;
+      mesh->data->vertices.reset_used(0);
+      mesh->data->vertex_normals.reset_used(0);
+      mesh->data->vertex_tex_coords.reset_used(0);
+      mesh->data->vertex_colors.reset_used(0);
+      mesh->data->faces.reset_used(0);
+
+      // 1. find out the minima and maxima of the mesh
+      vsx_vector minima;
+      vsx_vector maxima;
+
+
+      vsx_vector* vs_p;
+      unsigned long end = (*p)->data->vertices.size();
+      vs_p = &(*p)->data->vertices[0];
+      mesh->data->vertices.allocate(end);
+      mesh->data->vertices.reset_used(end);
+      vsx_vector* vs_d = mesh->data->vertices.get_pointer();
+
+
+      for (unsigned int i = 0; i < end; i++)
+      {
+        if (vs_p[i].x < minima.x) minima.x = vs_p[i].x;
+        if (vs_p[i].x > maxima.x) maxima.x = vs_p[i].x;
+
+        if (vs_p[i].y < minima.y) minima.y = vs_p[i].y;
+        if (vs_p[i].y > maxima.y) maxima.y = vs_p[i].y;
+
+        if (vs_p[i].z < minima.z) minima.z = vs_p[i].z;
+        if (vs_p[i].z > maxima.z) maxima.z = vs_p[i].z;
+      }
+
+      // find largest diff
+      float diff = fabs(maxima.x - minima.x);
+      if (fabs(maxima.y - minima.y) > diff) diff = fabs(maxima.y - minima.y);
+      if (fabs(maxima.z - minima.z) > diff) diff = fabs(maxima.z - minima.z);
+
+      if (diff == 0.0f) diff = 1.0f;
+
+      float scaling = 1.0f / diff;
+
+
+      float xmove = -minima.x * scaling;
+      float ymove = -minima.y * scaling;
+      float zmove = -minima.z * scaling;
+
+
+      vs_p = &(*p)->data->vertices[0];
+      for (unsigned int i = 0; i < end; i++)
+      {
+        vs_d[i] = vs_p[i] * scaling + vsx_vector(xmove, ymove, zmove);
+      }
+      //vsx_array<vsx_vector> vertices;
+      //vsx_array<vsx_vector> vertex_normals;
+      //vsx_array<vsx_color> vertex_colors;
+      //vsx_array<vsx_tex_coord> vertex_tex_coords;
+      //vsx_array<vsx_face> faces;
+
+
+      //if (prev_timestamp != (*p)->timestamp)
+      //{
+      mesh->data->vertex_normals.set_volatile();
+      mesh->data->vertex_normals.set_data((*p)->data->vertex_normals.get_pointer(), (*p)->data->vertex_normals.size());
+
+      mesh->data->vertex_tex_coords.set_volatile();
+      mesh->data->vertex_tex_coords.set_data((*p)->data->vertex_tex_coords.get_pointer(), (*p)->data->vertex_tex_coords.size());
+
+      mesh->data->vertex_tangents.set_volatile();
+      mesh->data->vertex_tangents.set_data((*p)->data->vertex_tangents.get_pointer(), (*p)->data->vertex_tangents.size());
+
+      mesh->data->vertex_colors.set_volatile();
+      mesh->data->vertex_colors.set_data((*p)->data->vertex_colors.get_pointer(), (*p)->data->vertex_colors.size());
+
+      mesh->data->faces.set_volatile();
+      mesh->data->faces.set_data((*p)->data->faces.get_pointer(), (*p)->data->faces.size());
+//      }
+
+
+      //for (unsigned int i = 0; i < (*p)->data->vertex_normals.size(); i++) mesh->data->vertex_normals[i] = (*p)->data->vertex_normals[i];
+      //for (unsigned int i = 0; i < (*p)->data->vertex_tangents.size(); i++) mesh->data->vertex_tangents[i] = (*p)->data->vertex_tangents[i];
+      //for (unsigned int i = 0; i < (*p)->data->vertex_tex_coords.size(); i++) mesh->data->vertex_tex_coords[i] = (*p)->data->vertex_tex_coords[i];
+      //for (int i = 0; i < (*p)->data->vertex_normals.size(); i++) mesh->data->vertex_normals[i] = (*p)->data->vertex_normals[i];
+      //for (unsigned int i = 0; i < (*p)->data->vertex_colors.size(); i++) mesh->data->vertex_colors[i] = (*p)->data->vertex_colors[i];
+      //for (unsigned int i = 0; i < (*p)->data->faces.size(); i++) mesh->data->faces[i] = (*p)->data->faces[i];
+      mesh->timestamp++;
+      mesh_out->set_p(mesh);
+      //for (int i = 0; i < (*p)->data->vertex_normals.size(); i++) mesh->data->vertex_normals[i] = (*p)->data->vertex_normals[i];
+      param_updates = 0;
+    }
+  }
+};
+
 class vsx_module_mesh_translate_edge_wraparound : public vsx_module {
   // in
   vsx_module_param_mesh* mesh_in;
@@ -2604,6 +2742,7 @@ vsx_module* create_new_module(unsigned long module) {
     case 14: return (vsx_module*)(new vsx_module_mesh_vertex_distance_sort);
     case 15: return (vsx_module*)(new vsx_module_mesh_translate_edge_wraparound);
     case 16: return (vsx_module*)(new vsx_module_mesh_vortex);
+    case 17: return (vsx_module*)(new vsx_module_mesh_scale_normalize);
   }
   return 0;
 }
@@ -2628,9 +2767,10 @@ void destroy_module(vsx_module* m,unsigned long module) {
     case 14: delete (vsx_module_mesh_vertex_distance_sort*)m; break;
     case 15: delete (vsx_module_mesh_translate_edge_wraparound*)m; break;
     case 16: delete (vsx_module_mesh_vortex*)m; break;
+    case 17: delete (vsx_module_mesh_scale_normalize*)m; break;
   }
 }
 
 unsigned long get_num_modules() {
-  return 17;
+  return 18;
 }
