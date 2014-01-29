3efe4620a74b9d92bccc13ad781f27413811ec7e
Jonatan "jaw" Wallmander <jonatan@vovoid.com>
2014-01-29 18:39:22 +0100

    New command line argument to artiste "-ff" to start in fullwindow mode

--- artiste/src/vsx_widget/helpers/vsx_widget_preview.cpp
+++ artiste/src/vsx_widget/helpers/vsx_widget_preview.cpp
@@ -39,89 +39,99 @@
 
 void vsx_window_texture_viewer::draw_2d()
 {
-	visible = !performance_mode;
+  visible = !performance_mode;
+
   if (performance_mode)
     return;
 
-    frame_delta += dtime;
-    ++frame_count;
-    if (frame_count == 50) {
-      fps = 1/(frame_delta/frame_count);
-      frame_count = 0;
-      frame_delta = 0;
-    }
-    title = "vsxu preview (slow) @ "+i2s((int)round(fps))+" fps, Ctrl+F(ullscreen)";
-    float vis = visible;
-    if (!visible) color.a = 0.3; else color.a = 1;
-    visible = 1;
-    vsx_widget_window::draw_2d();
-    visible = vis;
-    pos_.y = pos.y+dragborder;
-    size_.y = size.y-font_size-dragborder;
-    pos_.x = pos.x+dragborder;
-    size_.x = size.x-dragborder*2;
+  frame_delta += dtime;
+  ++frame_count;
 
-      if (visible)
-      glColor4f(0,0,0,1);
-      else
-      glColor4f(0,0,0,0.3);
+  if (frame_count == 50)
+  {
+    fps = 1/(frame_delta/frame_count);
+    frame_count = 0;
+    frame_delta = 0;
+  }
 
-      glMatrixMode(GL_PROJECTION);
-      glLoadIdentity();
-      glMatrixMode(GL_MODELVIEW);
-      glLoadIdentity();
-      if (*engine == 0) {
-      	set_pos(vsx_vector(0.0,2.0,0.0));
-      	set_size(vsx_vector(0.0,0.0,0.0));
-      }
-      // set new viewport
-      if (run)
-      if (*engine)
-      {
-        // get viewport
-        GLint	viewport[4];
-        vsx_gl_state::get_instance()->viewport_get(viewport);
-        vsx_gl_state::get_instance()->viewport_set(
-          (int)ceil(pos_.x*(screen_x-1)),
-          (int)ceil((pos_.y)*(screen_y-1)),
-          (int)ceil(size_.x*(screen_x-1)),
-          (int)ceil((size_.y)*(screen_y-1))
-        );
+  title = "vsxu preview (slow) @ "+i2s((int)round(fps))+" fps, Ctrl+F(ullscreen)";
+  float vis = visible;
 
-        glScissor(
-          (int)ceil(pos_.x*(screen_x-1)),
-          (int)ceil((pos_.y)*(screen_y-1)),
-          (int)ceil(size_.x*(screen_x-1)),
-          (int)ceil(size_.y*(screen_y-1))
-        );
+  if (!visible)
+    color.a = 0.3;
+  else
+    color.a = 1;
 
-        glEnable(GL_SCISSOR_TEST);
+  visible = 1;
+  vsx_widget_window::draw_2d();
+  visible = vis;
+  pos_.y = pos.y+dragborder;
+  size_.y = size.y-font_size-dragborder;
+  pos_.x = pos.x+dragborder;
+  size_.x = size.x-dragborder*2;
 
-        // render the engine
-        ((vsx_engine*)(*engine))->render();
+  if (visible)
+    glColor4f(0,0,0,1);
+  else
+    glColor4f(0,0,0,0.3);
 
-        glPolygonMode(GL_FRONT, GL_FILL);
-        glPolygonMode(GL_BACK, GL_FILL);
-        glDisable(GL_POLYGON_SMOOTH);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+  if (*engine == 0)
+  {
+    set_pos(vsx_vector(0.0,2.0,0.0));
+    set_size(vsx_vector(0.0,0.0,0.0));
+  }
+  // set new viewport
+  if (run)
+  if (*engine)
+  {
+    // get viewport
+    GLint	viewport[4];
+    vsx_gl_state::get_instance()->viewport_get(viewport);
+    vsx_gl_state::get_instance()->viewport_set(
+      (int)ceil(pos_.x*(screen_x-1)),
+      (int)ceil((pos_.y)*(screen_y-1)),
+      (int)ceil(size_.x*(screen_x-1)),
+      (int)ceil((size_.y)*(screen_y-1))
+    );
+
+    glScissor(
+      (int)ceil(pos_.x*(screen_x-1)),
+      (int)ceil((pos_.y)*(screen_y-1)),
+      (int)ceil(size_.x*(screen_x-1)),
+      (int)ceil(size_.y*(screen_y-1))
+    );
+
+    glEnable(GL_SCISSOR_TEST);
+
+    // render the engine
+    ((vsx_engine*)(*engine))->render();
+
+    glPolygonMode(GL_FRONT, GL_FILL);
+    glPolygonMode(GL_BACK, GL_FILL);
+    glDisable(GL_POLYGON_SMOOTH);
 
 
-        // reset the viewport
-        vsx_gl_state::get_instance()->viewport_set(viewport[0],viewport[1],viewport[2],viewport[3]);
+    // reset the viewport
+    vsx_gl_state::get_instance()->viewport_set(viewport[0],viewport[1],viewport[2],viewport[3]);
 
-        glScissor(viewport[0],viewport[1],viewport[2],viewport[3]);
-        glDisable(GL_SCISSOR_TEST);
-      }
-      glDepthMask(GL_TRUE);
-      glDisable(GL_DEPTH_TEST);
-    	glMatrixMode(GL_PROJECTION);
-      glLoadIdentity();
-      gluOrtho2D(0, 1, 0, 1);
-      glMatrixMode(GL_MODELVIEW);
-      glLoadIdentity();
-      glEnable (GL_BLEND);
-      glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glScissor(viewport[0],viewport[1],viewport[2],viewport[3]);
+    glDisable(GL_SCISSOR_TEST);
+  }
+  glDepthMask(GL_TRUE);
+  glDisable(GL_DEPTH_TEST);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluOrtho2D(0, 1, 0, 1);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+  glEnable (GL_BLEND);
+  glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    draw_children_2d();
+  draw_children_2d();
 }
 
 bool vsx_window_texture_viewer::event_key_down(signed long key, bool alt, bool ctrl, bool shift)
@@ -129,70 +139,62 @@ bool vsx_window_texture_viewer::event_key_down(signed long key, bool alt, bool c
   VSX_UNUSED(alt);
   VSX_UNUSED(shift);
 
-  if (ctrl && key == 32) { run = !run; return false;}
-  if (key == 32) { run = !run; return false;}
-  if (key == 13) {fullwindow = !fullwindow; if (fullwindow) mouse.hide_cursor(); else mouse.show_cursor();}
+  if (ctrl && key == 32)
+  {
+    run = !run;
+    return false;
+  }
+
+  if (key == 32)
+  {
+    run = !run;
+    return false;
+  }
+
+  if (key == 13)
+  {
+    fullwindow = !fullwindow;
+  }
   return false;
 }
 
-void vsx_window_texture_viewer::command_process_back_queue(vsx_command_s *t) {
-//  printf("command: %s\n",t->cmd.c_str());
-  if (t->cmd == "fullwindow") fullwindow = !fullwindow; else
-  if (t->cmd == "toggle") run = !run; else
-  vsx_widget::command_process_back_queue(t);
+void vsx_window_texture_viewer::command_process_back_queue(vsx_command_s *t)
+{
+  if (t->cmd == "fullwindow")
+  {
+    fullwindow = !fullwindow;
+    return;
+  }
 
-//  printf("run %i\n",run);
+  if (t->cmd == "toggle")
+  {
+    run = !run;
+    return;
+  }
+
+  vsx_widget::command_process_back_queue(t);
 }
 
-void vsx_window_texture_viewer::init() {
-  if (init_run) return;
-/*  menu = add(new vsx_widget_popup_menu,".obtx_menu");
-  menu->commands.adds(VSX_COMMAND_MENU, "disable/enable {space or ctrl+space globally}", "toggle","");
-  menu->commands.adds(VSX_COMMAND_MENU, "toggle fullwindow {enter or ctrl+f globally}", "fullwindow","");
-  menu->commands.adds(VSX_COMMAND_MENU, "toggle fullscreen", "fullscreen","");
-  modestring_commands.reset();
-  vsx_command_s* c;
-  menu->commands.adds(VSX_COMMAND_MENU, "fullscreen (current resolution) {alt+enter}", "fullscreen","");
+void vsx_window_texture_viewer::init()
+{
+  if (init_run)
+    return;
 
-  while (c = modestring_commands.get()) {
-    menu->commands.adds(VSX_COMMAND_MENU, "fullscreen "+c->cmd_data, "fullscreen",c->cmd_data);
-  }
-  //menu->commands.adds(VSX_COMMAND_MENU, "fullscreen 800x600:32@60 {alt+1}", "fullscreen","800x600:32@60");
-  //menu->commands.adds(VSX_COMMAND_MENU, "fullscreen 800x600:32@85 {alt+enter}", "fullscreen","800x600:32@85");
-  //menu->commands.adds(VSX_COMMAND_MENU, "fullscreen 1024x768:32@60 {alt+2}", "fullscreen","1024x768:32@60");
-  //menu->commands.adds(VSX_COMMAND_MENU, "fullscreen 1024x768:32@85", "fullscreen","1024x768:32@85");
-  //menu->commands.adds(VSX_COMMAND_MENU, "fullscreen 1280x800:32@60", "fullscreen","1280x800:32@60");
-  //menu->commands.adds(VSX_COMMAND_MENU, "fullscreen 1280x800:32@85", "fullscreen","1280x800:32@85");
-  //menu->commands.adds(VSX_COMMAND_MENU, "fullscreen 1280x1024:32@85", "fullscreen","1280x1024:32@85");
-  menu->commands.adds(VSX_COMMAND_MENU, "custom modestring", "custom","");
-  menu->size.x = 0.35;
-  menu->size.y = 0.5;*/
   init_children();
-  /*pos_.y = pos.y-font_size+dragborder*2;
-  size_.y = size.y-font_size+dragborder*2;
-  pos_.x = pos.x+dragborder;
-  size_.x = size.x-dragborder*2;*/
 
-  if (!init_run) {
-  	vsx_widget_window::init();
-  }
+  vsx_widget_window::init();
   topmost = true;
-//  texture = 0;
   title = "vsxu output preview";
   allow_resize_x = true;
   allow_resize_y = true;
-  //dragborder = 0.001;
-  //pos.y = 1;
-  //pos.x = 0;
-  //size.x = 0.3;
-  //size.y = 0.3;
-  //target_size = size;
-  //target_pos = pos;
   visible = 1;
   size_min.x = 0.2;
   size_min.y = 0.15;
-
   init_run = true;
+
+  // start in fullwindow mode (from command line arguments)
+  if (vsx_argvector::get_instance()->has_param("ff") )
+    fullwindow = true;
 }
 
 void vsx_window_texture_viewer::set_server(vsx_widget* new_server)
@@ -201,10 +203,10 @@ void vsx_window_texture_viewer::set_server(vsx_widget* new_server)
   engine = & ((vsx_widget_server*)new_server)->engine;
 }
 
-vsx_window_texture_viewer::vsx_window_texture_viewer() {
-  // vsxu gui production control mechanisms
+vsx_window_texture_viewer::vsx_window_texture_viewer()
+{
   fps = 0.0;
-  engine = 0;
+  engine = 0x0;
   frame_count = 0;
   frame_delta = 0;
   init_run = false;
