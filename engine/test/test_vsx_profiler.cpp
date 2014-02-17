a22b06ee14685b55daa3adf025b1e77713ecb62b
Jonatan "jaw" Wallmander <jonatan@vovoid.com>
2014-02-17 22:16:50 +0100

    Small cleanups in test_vsx_profiler + reference for cpu cores info

--- engine/test/test_vsx_profiler.cpp
+++ engine/test/test_vsx_profiler.cpp
@@ -1,36 +1,21 @@
 #include <inttypes.h>
-#include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sched.h>
 #include <linux/unistd.h>
 #include <sys/syscall.h>
-#include <errno.h>
-#include "../include/vsx_fifo.h"
+
 #include "../include/vsx_profiler.h"
 
-#define INC_TO 1000000
-
-vsx_fifo<> fifo;
-
-int producer_still_running = 1;
-int64_t producer_sum;
-int64_t consumer_sum;
-
-uint64_t producer_start;
-uint64_t consumer_end;
-int64_t num_consumed = 0;
-
-volatile __attribute__((aligned(64))) int64_t run_thread = 1;
+volatile __attribute__((aligned(64))) int64_t run_threads = 1;
 
 void *thread_producer( void *arg )
 {
   VSX_UNUSED(arg);
   vsx_profiler* p = vsx_profiler_manager::get_instance()->get_profiler();
-  while ( __sync_fetch_and_add( &run_thread, 0) )
+  while ( __sync_fetch_and_add( &run_threads, 0) )
   {
-    vsx_printf("running producer1 %d\n", run_thread);
     p->begin("1 test1");
     usleep(20);
     p->begin("1 test2");
@@ -47,9 +32,8 @@ void *thread_producer2( void *arg )
 {
   VSX_UNUSED(arg);
   vsx_profiler* p = vsx_profiler_manager::get_instance()->get_profiler();
-  while ( __sync_fetch_and_add( &run_thread, 0) )
+  while ( __sync_fetch_and_add( &run_threads, 0) )
   {
-    vsx_printf("running producer2 %d\n", run_thread);
     p->begin("2 test1");
     usleep(100);
     p->begin("2 test2");
@@ -79,6 +63,8 @@ int main()
     return -1;
   }
 
+  vsx_printf("This system has %d cores available\n", procs);
+
   thrs = (pthread_t*)malloc( sizeof( pthread_t ) * procs );
   if (thrs == NULL)
   {
@@ -103,12 +89,11 @@ int main()
 
   sleep( 2 );
 
-  __sync_fetch_and_sub( &run_thread, 1);
+  __sync_fetch_and_sub( &run_threads, 1);
 
 
   for (i = 0; i < 2; i++)
   {
-    vsx_printf("joining iterator %d\n", i);
     pthread_join( thrs[i], NULL );
   }
 
