2335edc2144399c9c2e5f3061d1e838a9d6634e6
bofh19 <theshadowwarrior88@gmail.com>
2014-02-23 18:10:18 +0530

    using platform_family instead of platform

--- server/src/main/linux-opengl/main.cpp
+++ server/src/main/linux-opengl/main.cpp
@@ -32,10 +32,7 @@
 #include "vsxfst.h"
 #include "vsx_version.h"
 #include "vsx_timer.h"
-#if PLATFORM == PLATFORM_LINUX
-#include <unistd.h>
-#endif
-#if PLATFORM == PLATFORM_MACINTOSH
+#if PLATFORM_FAMILY == PLATFORM_FAMILY_UNIX
 #include <unistd.h>
 #endif
 #include <stdlib.h>
