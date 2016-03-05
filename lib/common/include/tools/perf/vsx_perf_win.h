1d8c751d8c5104d0e942b275b2cff27ed79a5f78
Jonatan "jaw" Wallmander <jonatan@vovoid.com>
2016-03-05 14:38:44 +0100

    Perf tools for Linux

---
+++ lib/common/include/tools/perf/vsx_perf_win.h
@@ -0,0 +1,43 @@
+#pragma once
+class vsx_perf
+{
+
+public:
+
+  // cache misses
+  void cache_misses_start()
+  {
+  }
+
+  void cache_misses_begin()
+  {
+  }
+
+  long long cache_misses_end()
+  {
+    return 0;
+  }
+
+  void cache_misses_stop()
+  {
+  }
+
+  // cpu instructions
+  void cpu_instructions_start()
+  {
+  }
+
+  void cpu_instructions_begin()
+  {
+  }
+
+  long long cpu_instructions_end()
+  {
+    return 0;
+  }
+
+  void cpu_instructions_stop()
+  {
+  }
+
+};
