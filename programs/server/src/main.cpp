c0f547e5569e39923d8a5f8e3d0b0053aad88953
Jonatan "jaw" Wallmander <jonatan@vovoid.com>
2016-03-23 09:38:04 +0100

    Server ported to new application system

--- programs/server/src/main.cpp
+++ programs/server/src/main.cpp
@@ -21,47 +21,19 @@
 * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
-#ifndef VSX_APPLICATION_H
-#define VSX_APPLICATION_H
+#include <vsx_version.h>
+#include <vsx_platform.h>
 
-#include <sys/time.h>
+#include "server_application.h"
 
+#include <vsx_application_manager.h>
+#include <vsx_application_run.h>
 
-// GL state
-#include "vsx_gl_state.h"
-
-//extern vsx_engine* vme;
-//#define vsxu_version "VSXu 0.1.14 beta"
-//extern vsx_string<>vsxu_name;
-//extern vsx_string<>vsxu_version;
-//extern char* vsxu_ver;
-
-
-extern bool app_ctrl;
-extern bool app_alt;
-extern bool app_shift;
-extern bool dual_monitor;
-
-void app_init(int id);
-
-void app_print_cli_help();
-
-void app_pre_draw();
-
-bool app_draw(int id);
-
-void app_char(long key);
-void app_key_down(long key);
-void app_key_up(long key);
-
-void app_mouse_move_passive(int x, int y);
-void app_mouse_move(int x, int y);
-
-// buttons: 0 = left, 1 = middle, 2 = right
-void app_mouse_down(unsigned long button,int x,int y);
-void app_mouse_up(unsigned long button,int x,int y);
-
-// -1 to -5 or whatever up to +1
-void app_mousewheel(float diff,int x,int y);
-
-#endif
+int main(int argc, char* argv[])
+{
+  vsx_argvector::get_instance()->init_from_argc_argv(argc, argv);
+  server_application application;
+  vsx_application_manager::get_instance()->application_set(&application);
+  vsx_application_run::run();
+  return 0;
+}
---
+++ programs/server/src/server_application.h
@@ -0,0 +1,83 @@
+/**
+* Project: VSXu: Realtime modular visual programming language, music/audio visualizer.
+*
+* This file is part of Vovoid VSXu.
+*
+* @author Jonatan Wallmander, Vovoid Media Technologies AB Copyright (C) 2003-2013
+* @see The GNU Public License (GPL)
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+* or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+* for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program; if not, write to the Free Software Foundation, Inc.,
+* 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+#pragma once
+
+#include <vsx_version.h>
+#include <vsx_application.h>
+#include <vsx_application_control.h>
+#include <vsx_application_input_state_manager.h>
+
+#include <vsx_engine.h>
+#include <vsx_command_client_server.h>
+
+class server_application
+    : public vsx_application
+{
+  vsx_engine* vxe = 0x0;
+  vsx_module_list_abs* module_list = 0x0;
+  vsx_command_list_server cl_server;
+
+  vsx_command_list internal_cmd_in;
+  vsx_command_list internal_cmd_out;
+
+public:
+
+  vsx_string<> window_title_get()
+  {
+    char titlestr[ 200 ];
+    sprintf( titlestr, "Vovoid VSXu Server %s [%s %d-bit] %s", VSXU_VER, PLATFORM_NAME, PLATFORM_BITS, VSXU_VERSION_COPYRIGHT);
+    return vsx_string<>(titlestr);
+  }
+
+  void init()
+  {
+    module_list = vsx_module_list_factory_create();
+    vxe = new vsx_engine(module_list);
+    vxe->start();
+    cl_server.set_command_lists(&internal_cmd_in,&internal_cmd_out);
+    cl_server.start();
+  }
+
+  void uninit()
+  {
+    vxe->stop();
+    delete vxe;
+    vsx_module_list_factory_destroy(module_list);
+  }
+
+  void draw()
+  {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    vxe->process_message_queue(&internal_cmd_in,&internal_cmd_out);
+    vxe->render();
+  }
+
+  void event_key_down(long key)
+  {
+    if (key == VSX_SCANCODE_ESCAPE)
+      vsx_application_control::get_instance()->shutdown_request();
+  }
+
+};
+
