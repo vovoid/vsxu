876c9ffd1a1b9a0ea0c2e43a63efe623e1e57d51
Jonatan "jaw" Wallmander <jonatan@vovoid.com>
2016-07-11 21:55:56 +0200

    Convenience function to set vsx color from percentages

--- lib/common/include/color/vsx_color.h
+++ lib/common/include/color/vsx_color.h
@@ -140,6 +140,14 @@ public:
     a=ia;
   }
 
+  void set_percentages(const T rp, const T gp, const T bp, const T ap)
+  {
+    r = rp * 0.01;
+    g = gp * 0.01;
+    b = bp * 0.01;
+    a = ap * 0.01;
+  }
+
 
   inline vsx_color operator +(const vsx_color &t)
   {
