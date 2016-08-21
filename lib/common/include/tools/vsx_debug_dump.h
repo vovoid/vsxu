86c5f0664928c3af2a43b4b0dbbf31b63e2c2583
Jonatan "jaw" Wallmander <jonatan@vovoid.com>
2016-08-21 11:02:19 +0200

    Started work on a helper file for dumping out various variable types to stdout

---
+++ lib/common/include/tools/vsx_debug_dump.h
@@ -0,0 +1,21 @@
+#pragma once
+
+inline void debug_dump_value(double& v, vsx_string<> name)
+{
+  vsx_printf(L"%s: %f\n", name.c_str(), v);
+}
+
+inline void debug_dump_value(float& v, vsx_string<> name)
+{
+  vsx_printf(L"%s: %f\n", name.c_str(), v);
+}
+
+
+inline void debug_dump_value(vsx_vector2d& v, vsx_string<> name)
+{
+  vsx_printf(L"%s: %f, %f\n", name.c_str(), v.x, v.y);
+}
+
+
+#define VSX_DUMP(v) debug_dump_value(v, #v );
+
