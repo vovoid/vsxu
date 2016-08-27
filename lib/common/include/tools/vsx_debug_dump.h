c1fa27b2986f9dfe779230676cd769b19423a3c1
Jonatan "jaw" Wallmander <jonatan@vovoid.com>
2016-08-27 12:31:01 +0200

    added vsx dump for vsx_vector3

--- lib/common/include/tools/vsx_debug_dump.h
+++ lib/common/include/tools/vsx_debug_dump.h
@@ -16,6 +16,11 @@ inline void debug_dump_value(vsx_vector2d& v, vsx_string<> name)
   vsx_printf(L"%s: %f, %f\n", name.c_str(), v.x, v.y);
 }
 
+inline void debug_dump_value(vsx_vector3d& v, vsx_string<> name)
+{
+  vsx_printf(L"%s: %f, %f, %f\n", name.c_str(), v.x, v.y);
+}
+
 
 #define VSX_DUMP(v) debug_dump_value(v, #v );
 
