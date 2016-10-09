ff3d3bdd5ea1a8ae7f096ad2760915cb9b6b9779
Jonatan "jaw" Wallmander <jonatan@vovoid.com>
2016-10-09 20:23:35 +0200

    more types supported in debug dump

--- lib/common/include/tools/vsx_debug_dump.h
+++ lib/common/include/tools/vsx_debug_dump.h
@@ -10,6 +10,12 @@ inline void debug_dump_value(float& v, vsx_string<> name)
   vsx_printf(L"%s: %f\n", name.c_str(), v);
 }
 
+inline void debug_dump_value(vsx_vector2f& v, vsx_string<> name)
+{
+  vsx_printf(L"%s: %f, %f\n", name.c_str(), v.x, v.y);
+}
+
+
 
 inline void debug_dump_value(vsx_vector2d& v, vsx_string<> name)
 {
