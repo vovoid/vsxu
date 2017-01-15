acde81f4dc341fb09425e2d3fe58691d27209b2c
Jonatan "jaw" Wallmander <jonatan@vovoid.com>
2017-01-15 19:48:07 +0100

    Cache leak issue fixed in texgen blob

--- plugins/src/texture/module_texture_texgen_blob.h
+++ plugins/src/texture/module_texture_texgen_blob.h
@@ -155,8 +155,11 @@ public:
 
     if (vsx_texture_gl_cache::get_instance()->has(cache_handle, 0, hint))
     {
+      if (texture)
+        texture_old = texture;
+
       if (!texture)
-        texture = new vsx_texture<>;
+        texture = new vsx_texture<>(true);
 
       texture->texture = vsx_texture_gl_cache::get_instance()->aquire(cache_handle, engine_state->filesystem, false, 0, hint, false );
       texture_out->set(texture);
