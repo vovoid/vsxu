d5a07a5c9830c7bde524293ce041f184fc349e6e
Jonatan "jaw" Wallmander <jonatan@vovoid.com>
2017-03-19 12:30:33 +0100

    Split countdown event into dt and et

---
+++ lib/common/include/tools/vsx_countdown_event_dt.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <math/vsx_rand_singleton.h>
+#include <tools/vsx_req.h>
+#include <functional>
+
+namespace vsx
+{
+
+/**
+ * Triggers function when timer has counted down.
+ */
+template<size_t min_time_ms, size_t max_time_ms>
+class countdown_event_dt
+{
+  float value = 0.0f;
+
+public:
+
+  countdown_event_dt()
+  {
+    value = (float)min_time_ms * 0.001f + (float)(max_time_ms - min_time_ms) * 0.001f * vsx_rand_singleton::get()->rand.frand();
+  }
+
+  /**
+   * @brief on_countdown_rand
+   * @param dt  Delta time (per frame)
+   * @param action
+   */
+  void on_countdown_rand(float dt, std::function<void()> action)
+  {
+    value -= dt;
+    req(value < 0.0f);
+    value = (float)min_time_ms * 0.001f  + vsx_rand_singleton::get()->rand.frand() * (float)(max_time_ms - min_time_ms) * 0.001f;
+    action();
+  }
+
+};
+
+}
--- lib/common/include/tools/vsx_countdown_event_et.h
+++ lib/common/include/tools/vsx_countdown_event_et.h
@@ -7,21 +7,36 @@
 namespace vsx
 {
 
+/**
+ * Triggers function when timer has counted down.
+ */
 template<size_t min_time_ms, size_t max_time_ms>
-class countdown_event
+class countdown_event_et
 {
   float value = 0.0f;
-
+  float et_prev = 0.0f;
 public:
 
-  countdown_event()
+  countdown_event_et()
   {
     value = (float)min_time_ms * 0.001f + (float)(max_time_ms - min_time_ms) * 0.001f * vsx_rand_singleton::get()->rand.frand();
   }
 
-  void on_countdown_rand(float dt, std::function<void()> action)
+  /**
+   * @brief on_countdown_rand
+   * @param et  Elapsed time (seconds) since program has started
+   * @param action
+   */
+  void on_countdown_rand(float et, std::function<void()> action)
   {
-    value -= dt;
+//    if (et_prev < 0.0)
+//    {
+//      et_prev = et;
+//      return;
+//    }
+
+    value -= (et - et_prev);
+    et_prev = et;
     req(value < 0.0f);
     value = (float)min_time_ms * 0.001f  + vsx_rand_singleton::get()->rand.frand() * (float)(max_time_ms - min_time_ms) * 0.001f;
     action();
