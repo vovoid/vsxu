31638b1e8c56a58c69f754baf2f8e2a5c9062e49
Jonatan "jaw" Wallmander <jonatan@vovoid.com>
2018-01-31 15:50:32 +0100

    templatized counter class

---
+++ lib/common/include/tools/vsx_count_up.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <inttypes.h>
+
+namespace vsx
+{
+template <size_t start, size_t max, bool wrap>
+class count_up
+{
+  size_t value = start;
+
+public:
+
+  size_t get_and_increase()
+  {
+    size_t cur = value;
+    value++;
+    if (wrap)
+      if (value > max)
+        value = 0;
+    return cur;
+  }
+
+};
+}
